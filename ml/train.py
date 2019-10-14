914621379ca76d57910a1b7805b27f1de24f62f7
LZL96 <zuolin.liu.2017@sis.smu.edu.sg>
2019-10-14 14:37:02 +0800

    added some models

--- ml/train.py
+++ ml/train.py
@@ -1,18 +1,24 @@
 from helper import *
-from helper.text_preprocessor import *
 from preprocess import *
 
 cp("importing modules")
 
-x_train, x_test, y_train, y_test = get_tts_xy("training_data/train2.csv"); cp("preprocessing")
+NUM = 5
+
+x_train, x_test, y_train, y_test = get_vectorized_tts_xy(f"training_data/train{NUM}.csv"); cp("preprocessing")
 
 from sklearn.naive_bayes import BernoulliNB
 from sklearn.metrics import accuracy_score as acc
 
-nb = BernoulliNB()
-nb.fit(x_train, y_train)
-y_pred = nb.predict(x_test)
+model = BernoulliNB()
+model.fit(x_train, y_train)
+y_pred = model.predict(x_test)
 
 print(acc(y_pred, y_test))
+
+# saving model into "saved" folder 
+TARGET_NAME = str(model.__class__)[:-2].split(".")[-1] + "_" + str(NUM)
+
+dump((vec,model), f"{TARGET_NAME}")
 
 cpend()
