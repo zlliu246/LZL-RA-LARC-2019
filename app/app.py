0989fd58c63b5f73db52dc61371b07903ae4f9b9
LZL96 <zuolin.liu.2017@sis.smu.edu.sg>
2019-11-24 12:47:49 +0800

    removed ml

--- app/app.py
+++ app/app.py
@@ -1,14 +1,12 @@
 import json
 import os
 import sys
-import pickle
 
 import flask
 from flask import request
 from flask_cors import CORS
 
 from app.scrape import scrape_main 
-from ml.api import *
 
 app = flask.Flask(__name__)
 CORS(app)
@@ -35,21 +33,6 @@ def query_():
         from app/scrape/scrape_main.py
     """
     return flask.jsonify(scrape_main.search_google(query, num_results=num_results))
-
-vec, model = pickle.load(open("ml/saved/BernoulliNB_7.sav","rb"))
-
-@app.route("/api/classify")
-def classify_():
-    args = request.args
-    raw_query = args["query"]
-
-    query = get_vectorized_test_x([raw_query], vec)
-    pred = int(model.predict(query)[0])
-
-    return flask.jsonify({
-        "query": raw_query,
-        "relationship-related":{1:"yes", 0:"no"}[pred]
-    })
 
 if __name__ == '__main__':  
    app.run(debug=True, port="5000")
--- ml/api.py
+++
@@ -1,6 +0,0 @@
-from ml.helper.text_preprocessor import *
-
-def get_vectorized_test_x(lis, vec):
-    for i,val in enumerate(lis):
-        lis[i] = clean(val)
-    return vec.transform(lis)
